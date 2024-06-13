18bb2ebf80ed11eb24b06c9075af61592d179713
frenzie24 <kevincharlesgross@gmail.com>
2024-06-12 22:52:14 -0400

    added npm's uuid package for react li key generation

---
+++ client/src/utils/uuid.js
@@ -0,0 +1,7 @@
+//CREDIT TO UCF STAFF FOR HELPER SCRIPT
+
+// Immediately export a function that generates a string of random numbers and letters
+module.exports = () =>
+  Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
