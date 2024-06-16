fa30a314a4ef0ac37d4039c675a84481f3b5761e
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-06-16 14:08:42 -0400

    installed multer and path

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+const multer = require('multer');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
