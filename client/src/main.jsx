6a6d676d20032820844be536c611d52dc4bc16bd
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-06-17 00:03:39 -0400

    updated pages to run server and client simultaneously along with Apollo Provider wrap in main.jsx for testing pushing data

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
 import './index.css'
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </ApolloProvider>,
 )
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,75 +1,32 @@
-import { Navigate, useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
-import { Link } from "react-router-dom";
-
-import CharacterForm from "../components/CharacterForm";
-import CharacterList from "../components/CharacterList";
-
-import { QUERY_USER, QUERY_ME, QUERY_USER_CHARACTERS } from "../utils/queries";
-
-import Auth from "../utils/auth";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER_CHARACTERS } from '../utils/queries';
+import Auth from '../utils/auth';
 
 const Profile = () => {
-  const { username: userParam } = useParams();
+  const { userId } = useParams();
 
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam },
+  const { loading, data } = useQuery(QUERY_USER_CHARACTERS, {
+    variables: { userId: userId || Auth.getProfile().data._id },
   });
-
-  const user = data?.me || data?.user || {};
-
-  // Fetch characters owned by the logged-in user
-  const { loading: loadingCharacters, data: characterData } = useQuery(
-    QUERY_USER_CHARACTERS,
-    {
-      variables: { username: userParam || Auth.getProfile().data.username },
-    }
-  );
-
-  const characters = characterData?.userCharacters || [];
-
-  // Navigate to personal profile page if username is yours
-  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Navigate to='/me' />;
-  }
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
-  if (loadingCharacters) {
-    return <div>Loading Characters...</div>;
-  }
-
-  if (!user) {
-    return (
-      <h4>
-        You need to be logged in to see this. Use the navigation links above to
-        sign up or log in!
-      </h4>
-    );
-  }
-
-  if (characters.length === 0) {
-    return <h4>No characters to display</h4>;
-  }
+  const characters = data?.userCharacters || [];
 
   return (
     <div>
-      <div className='flex-row justify-center mb-3'>
-        <h2 className='col-12 col-md-10 bg-dark text-light p-3 mb-5'>
-          Viewing {userParam ? `${user.username}'s` : "your"} profile.
-        </h2>
-
-        <div className='col-12 col-md-10 mb-5'>
-          <CharacterList characters={characters} title='Characters' />
+      <h2>Your Characters</h2>
+      {characters.map((character) => (
+        <div key={character._id}>
+          <h3>{character.characterName}</h3>
+          <p>{character.characterClass}</p>
+          {/* Add other character fields here */}
         </div>
-        <div
-          className='col-12 col-md-10 mb-3 p-3'
-          style={{ border: "1px dotted #1a1a1a" }}
-        >
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
